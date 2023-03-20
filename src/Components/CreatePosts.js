91b7f6c65243bcaae8e90dd9b30bcd7a66584b91
Jkook1701 <bporwal@deqode.com>
2023-03-20 19:57:27 +0530

    Added Update & Delete feature

--- src/Components/CreatePosts.js
+++ src/Components/CreatePosts.js
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { createPost } from "../Redux/features/PostSlice";
 
 const CreatePosts = () => {
-    return(<div>CreatePosts</div>)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const [values, setValues] = useState({ title: "", body: "" })
+    const { title, body } = values;
+    const [displayPost, setDisplayPost] = useState(false)
+    const { loading, post } = useSelector(state => ({ ...state.app }))
+
+    const handlePost = (e) => {
+        e.preventDefault()
+        if (title) {
+            dispatch(createPost({ values }))
+            setValues({ title: "", body: "" })
+            setDisplayPost(true)
+        }
+        else {
+            window.alert("Enter Title for Your Post")
+        }
+    }
+
+    const publishPost = () => {
+        return (<>
+            {loading ? <h2>Loading...</h2> : (
+                <>
+                    {post.length > 0 &&
+                        <div className="postHolder">
+                            <p className="postTitle">{post[0].title}</p>
+                            <p className="postBody">{post[0].body}</p>
+                        </div>}
+                </>
+            )}
+        </>)
+    }
+    
+    return (<div>
+        <form>
+            <input
+                placeholder="Enter Post Title"
+                value={title}
+                onChange={(e) => setValues({ ...values, title: e.target.value })}
+            />
+            <textarea
+                placeholder="Create Post"
+                value={body}
+                onChange={(e) => setValues({ ...values, body: e.target.value })}
+            />
+            <button onClick={() => navigate("/")}>Back</button>
+            <button
+                type="submit"
+                onClick={handlePost}>Post</button>
+        </form>
+        <div>
+            {displayPost && publishPost()}
+        </div>
+    </div>)
 }
 
 export default CreatePosts;
--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -1,34 +1,88 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { getPost } from "../Redux/features/PostSlice";
+import { deletePost, getPost, setEditing, updatePost } from "../Redux/features/PostSlice";
 import './Posts.css'
 
 const Posts = () => {
-    const [id, setId] = useState()
+    const [id, setId] = useState(0)
+    const [postContent, setPostContent] = useState("")
     const navigate = useNavigate()
     const dispatch = useDispatch()
-    const {loading , post} = useSelector(state => ({...state.app}))
+    const { editing, deleting, loading, post, body } = useSelector(state => ({ ...state.app })) //app (reducer) from store 
+
+    useEffect(() => {
+        if (body) {
+            setPostContent(body)
+        }
+    }, [body])
 
     const handleFetch = (e) => {
         e.preventDefault()
-        if(id){
-            dispatch(getPost({id}))
+        if (id) {
+            dispatch(getPost({ id }))
             setId("")
         }
     }
+
+    const handleDelete = ({ id }) => {
+        dispatch(deletePost({ id: post[0].id }))
+    }
+
     return (<div>
         <label>Search Post by Post ID : </label>
         <input
             className="searchField"
             type="number"
             value={id}
-            onChange={(e)=> setId(e.target.value)}
+            onChange={(e) => setId(e.target.value)}
         />
         <div>
             <button type="submit" onClick={handleFetch}>Fetch Post</button>
             <button type="submit" onClick={() => navigate("/create")}>Create Post</button>
         </div>
+        {deleting && <h2>Deleting ...</h2>}
+        {
+            loading ? <h2>Loading ...</h2> : (
+                <>
+                    {post.length > 0 &&
+                        <div className="postHolder">
+                            <p className="postTitle">{post[0].title}</p>
+                            {
+                                editing ? (
+                                    <>
+                                        <textarea
+                                            value={postContent}
+                                            onChange={(e) => setPostContent(e.target.value)}
+                                        />
+                                        <div className="postButtons">
+                                            <button onClick={() => {
+                                                dispatch(updatePost({
+                                                    id: post[0].id,
+                                                    title: post[0].title,
+                                                    body: postContent
+                                                }))
+                                                dispatch(setEditing({ editing: false, body: "" })) //second dispatch to set the editing mode & empty body contents 
+                                            }}>Save</button>
+                                            <button onClick={() => {
+                                                dispatch(setEditing({ editing: false, body: "" }))
+                                            }}  >Cancel</button>
+
+                                        </div>
+                                    </>) :
+                                    <>
+                                        <p className="postBody">{post[0].body}</p>
+                                        <div className="postButtons">
+                                            <button onClick={() => dispatch(setEditing({ editing: true, body: post[0].body }))}>Edit</button>
+                                            <button onClick={handleDelete}>Delete</button>
+                                        </div>
+                                    </>
+
+                            }
+
+                        </div>}
+                </>)
+        }
     </div>)
 }
 
